4da830834f4db50858b4322db819efcfc19c4253
Sergio Rubio <rubiojr@rbel.co>
2024-08-30 18:42:26 +0200

    Revert typo fix

--- datasources/media/metadata_test.go
+++ datasources/media/metadata_test.go
@@ -118,7 +118,7 @@ func TestSplitCamelCaseIntoWords(t *testing.T) {
 		"GPSAltitudeRef":                   "GPS Altitude Ref",
 		"GPSAltitude":                      "GPS Altitude",
 		"GPSTimeStamp":                     "GPS Time Stamp",
-		"GPSSatelites":                     "GPS Satelites", // OK so it's misspelled in the EXIF data I encountered!
+		"GPSSatelites":                     "GPS Satellites", // OK so it's misspelled in the EXIF data I encountered!
 		"GPSSatellites":                    "GPS Satellites", // obviously, support the correct spelling too
 		"GPSStatus":                        "GPS Status",
 		"GPSMeasureMode":                   "GPS Measure Mode",
