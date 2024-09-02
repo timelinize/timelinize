2dbffa39b6b5cabf542aa5d66f964c9dc46c3309
Sergio Rubio <rubiojr@rbel.co>
2024-09-02 22:49:35 +0200

    Expand test

--- datasources/generic/generic_test.go
+++ datasources/generic/generic_test.go
@@ -9,23 +9,60 @@ import (
 
 func TestClientWalk(t *testing.T) {
 	client := &Client{}
-	itemChan := make(chan *timeline.Graph, 1)
-	defer close(itemChan)
-
 	ctx := context.Background()
 	opts := timeline.ListingOptions{DataSourceOptions: new(Options)}
 
-	client.walk(ctx, "testdata/fixtures/file.txt", ".", itemChan, opts)
-	graph := <-itemChan
-	item := graph.Item
-	if item.Content.Filename != "file.txt" {
-		t.Errorf("Expected filepath to be file.txt, got %s", item.Content.Filename)
-	}
+	t.Run("import single txt file", func(t *testing.T) {
+		itemChan := make(chan *timeline.Graph, 10)
+		defer close(itemChan)
 
-	client.walk(ctx, "testdata/fixtures", ".", itemChan, opts)
-	graph = <-itemChan
-	item = graph.Item
-	if item.Content.Filename != "file.txt" {
-		t.Errorf("Expected filepath to be file.txt, got %s", item.Content.Filename)
-	}
+		client.walk(ctx, "testdata/fixtures/file.txt", ".", itemChan, opts)
+		graph := <-itemChan
+		item := graph.Item
+		if item.Content.Filename != "file.txt" {
+			t.Errorf("Expected filepath to be file.txt, got %s", item.Content.Filename)
+		}
+	})
+
+	t.Run("import single zip file", func(t *testing.T) {
+		itemChan := make(chan *timeline.Graph, 10)
+		defer close(itemChan)
+
+		client.walk(ctx, "testdata/fixtures/file.zip", ".", itemChan, opts)
+		graph := <-itemChan
+		item := graph.Item
+		if item.Content.Filename != "file.txt" {
+			t.Errorf("Expected filepath to be file.txt, got %s", item.Content.Filename)
+		}
+	})
+
+	t.Run("Directory with single txt file", func(t *testing.T) {
+		itemChan := make(chan *timeline.Graph, 10)
+		defer close(itemChan)
+
+		client.walk(ctx, "testdata/fixtures/dir-single-file", ".", itemChan, opts)
+		graph := <-itemChan
+		item := graph.Item
+		if item.Content.Filename != "file.txt" {
+			t.Errorf("Expected filepath to be file.txt, got %s", item.Content.Filename)
+		}
+	})
+
+	t.Run("Directory with multiple files", func(t *testing.T) {
+		itemChan := make(chan *timeline.Graph, 10)
+		defer close(itemChan)
+		client.walk(ctx, "testdata/fixtures/dir-multiple-files", ".", itemChan, opts)
+
+		graph := <-itemChan
+		fname := graph.Item.Content.Filename
+		if fname != "file1" {
+			t.Errorf("Expected filepath to be file1, got %s", fname)
+		}
+
+		graph = <-itemChan
+		fname = graph.Item.Content.Filename
+		if fname != "file2" {
+			t.Errorf("Expected filepath to be file2, got %s", fname)
+		}
+	})
 }
