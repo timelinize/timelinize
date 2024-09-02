9865f9b4d6721c69a9c6e12e1b26ca7a76e6e29b
Sergio Rubio <rubiojr@rbel.co>
2024-09-02 21:58:02 +0200

    Clean test a bit

--- datasources/generic/generic_test.go
+++ datasources/generic/generic_test.go
@@ -12,15 +12,20 @@ func TestClientWalk(t *testing.T) {
 	itemChan := make(chan *timeline.Graph, 1)
 	defer close(itemChan)
 
-	client.walk(context.Background(), "testdata/fixtures/file.txt", ".", itemChan, timeline.ListingOptions{DataSourceOptions: new(Options)})
+	ctx := context.Background()
+	opts := timeline.ListingOptions{DataSourceOptions: new(Options)}
+
+	client.walk(ctx, "testdata/fixtures/file.txt", ".", itemChan, opts)
 	graph := <-itemChan
-	if graph.Item.Content.Filename != "file.txt" {
-		t.Errorf("Expected filepath to be file.txt, got %s", graph.Item.Content.Filename)
+	item := graph.Item
+	if item.Content.Filename != "file.txt" {
+		t.Errorf("Expected filepath to be file.txt, got %s", item.Content.Filename)
 	}
 
-	client.walk(context.Background(), "testdata/fixtures", ".", itemChan, timeline.ListingOptions{DataSourceOptions: new(Options)})
+	client.walk(ctx, "testdata/fixtures", ".", itemChan, opts)
 	graph = <-itemChan
-	if graph.Item.Content.Filename != "file.txt" {
-		t.Errorf("Expected filepath to be file.txt, got %s", graph.Item.Content.Filename)
+	item = graph.Item
+	if item.Content.Filename != "file.txt" {
+		t.Errorf("Expected filepath to be file.txt, got %s", item.Content.Filename)
 	}
 }
