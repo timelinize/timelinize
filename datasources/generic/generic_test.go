8c330cf55f13e68d97bf41fb79a387818b92164f
Sergio Rubio <rubiojr@rbel.co>
2024-09-04 20:56:51 +0200

    Simplify tests

--- datasources/generic/generic_test.go
+++ datasources/generic/generic_test.go
@@ -2,6 +2,8 @@ package generic
 
 import (
 	"context"
+	"slices"
+	"strings"
 	"testing"
 
 	"github.com/timelinize/timelinize/timeline"
@@ -14,62 +16,61 @@ func TestClientWalk(t *testing.T) {
 
 	t.Run("import single txt file", func(t *testing.T) {
 		itemChan := make(chan *timeline.Graph, 10)
-		defer close(itemChan)
 
 		client.FileImport(ctx, []string{"testdata/fixtures/file.txt"}, itemChan, opts)
-		fname := (<-itemChan).Item.Content.Filename
-		if fname != "file.txt" { //nolint:goconst
-			t.Errorf("Expected filepath to be file.txt, got %s", fname)
-		}
+		close(itemChan)
+
+		expectFiles(t, []string{"file.txt"}, itemChan)
 	})
 
 	t.Run("import single zip file", func(t *testing.T) {
 		itemChan := make(chan *timeline.Graph, 10)
-		defer close(itemChan)
 
 		client.FileImport(ctx, []string{"testdata/fixtures/file.zip"}, itemChan, opts)
-		fname := (<-itemChan).Item.Content.Filename
-		if fname != "file.txt" {
-			t.Errorf("Expected filepath to be file.txt, got %s", fname)
-		}
+		close(itemChan)
+
+		expectFiles(t, []string{"file.txt"}, itemChan)
 	})
 
 	t.Run("Directory with single txt file", func(t *testing.T) {
 		itemChan := make(chan *timeline.Graph, 10)
-		defer close(itemChan)
 
 		client.FileImport(ctx, []string{"testdata/fixtures/dir-single-file"}, itemChan, opts)
-		fname := (<-itemChan).Item.Content.Filename
-		if fname != "file.txt" {
-			t.Errorf("Expected filepath to be file.txt, got %s", fname)
-		}
+		close(itemChan)
+
+		expectFiles(t, []string{"file.txt"}, itemChan)
 	})
 
 	t.Run("Directory with multiple files", func(t *testing.T) {
 		itemChan := make(chan *timeline.Graph, 10)
-		defer close(itemChan)
 		client.FileImport(ctx, []string{"testdata/fixtures/dir-multiple-files"}, itemChan, opts)
+		close(itemChan)
 
-		fname := (<-itemChan).Item.Content.Filename
-		if fname != "file1" {
-			t.Errorf("Expected filepath to be file1, got %s", fname)
-		}
-
-		fname = (<-itemChan).Item.Content.Filename
-		if fname != "file2" {
-			t.Errorf("Expected filepath to be file2, got %s", fname)
-		}
+		expectFiles(t, []string{"file1", "file2"}, itemChan)
 	})
 
 	t.Run("Zip file within a zip file", func(t *testing.T) {
 		itemChan := make(chan *timeline.Graph, 10)
-		defer close(itemChan)
+
 		// zip-in-archive.zip contains file2.zip
 		client.FileImport(ctx, []string{"testdata/fixtures/zip-in-archive.zip"}, itemChan, opts)
+		close(itemChan)
 
-		fname := (<-itemChan).Item.Content.Filename
-		if fname != "file2.zip" {
-			t.Errorf("Expected filepath to be file2.zip, got %s", fname)
-		}
+		expectFiles(t, []string{"file2.zip"}, itemChan)
 	})
+}
+
+func expectFiles(t *testing.T, files []string, itemChan chan *timeline.Graph) {
+	graphs := []*timeline.Graph{}
+	for graph := range itemChan {
+		graphs = append(graphs, graph)
+		fname := graph.Item.Content.Filename
+		if !slices.Contains(files, graphs[0].Item.Content.Filename) {
+			t.Errorf("Expected file %s to be in %s", fname, strings.Join(files, ","))
+		}
+	}
+
+	if len(graphs) != len(files) {
+		t.Errorf("Expected %d files, got %d", len(files), len(graphs))
+	}
 }
