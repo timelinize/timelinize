cc9600ef6b9d8091bebc2c8fa25ba1f2da87faca
Sergio Rubio <rubiojr@rbel.co>
2024-09-04 14:18:17 +0200

    Rename data source directory also

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 	_ "github.com/timelinize/timelinize/datasources/facebook"
 	_ "github.com/timelinize/timelinize/datasources/generic"
 	_ "github.com/timelinize/timelinize/datasources/geojson"
-	_ "github.com/timelinize/timelinize/datasources/ghstars"
+	_ "github.com/timelinize/timelinize/datasources/github"
 	_ "github.com/timelinize/timelinize/datasources/googlelocation"
 	_ "github.com/timelinize/timelinize/datasources/googlephotos"
 	_ "github.com/timelinize/timelinize/datasources/gpx"
