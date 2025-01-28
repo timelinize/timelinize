b96e9836be594a3fc4a3bc390ace6249102a7085
Matthew Holt <mholt@users.noreply.github.com>
2025-01-27 22:17:51 -0700

    Yada yada

--- timeline/processing.go
+++ timeline/processing.go
@@ -255,9 +255,9 @@ func (p *processor) downloadGraphDataFiles(ctx context.Context, g *Graph, opts *
 			}
 		}()
 	}
-	if g.Item != nil && g.Item.Owner.NewPicture != nil {
-		// TODO: download the item owner's profile picture too, if available (though I don't know of anywhere this happens yet)
-	}
+	// TODO: download the item owner's profile picture too, if available (though I don't know of anywhere this happens yet)
+	// if g.Item != nil && g.Item.Owner.NewPicture != nil {
+	// }
 	for _, edge := range g.Edges {
 		if err := p.downloadGraphDataFiles(ctx, edge.From, opts); err != nil {
 			return err
