612cae9c0312134469e8e4962933bfd2e28612ef
Sergio Rubio <rubiojr@frameos.org>
2025-04-08 13:02:20 +0000

    Firefox data source (#55)
    * [WiP] Firefox data source
    
    Work in progress.
    
    Implements a new Firefox datasource capable of reading its
    places.sqlite database to import the browser history (page visits).
    
    The implementation currently has a number of issues:
    
    * Firefox (and Firefox based) browser keeps an exclusive lock on the
      places.sqlite database, and we can't dump or backup it while the
      browser is open, at least on Linux. To work around that,
      we copy the database to a temporary directory and import from it.
      This generally works, but isn't safe, as there's a risk of database
      corruption when doing the hot copy. Potential alternatives:
      * Ask the user to close the browser while the import happens, which
        isn't convenient/possible if this is happening regularly in the background.
      * Ignore and retry, as it'll eventually succeed, in the rare case the
        temporary db copy is corrupted and unreadable
      * Something else, no expert here.
    * You need to point Timelinize to the places.sqlite file directly. Pointing
      it to the Firefox profile directory doesn't seem to work, as it
      doesn't seem to scan recursively or list all the directory files and
      pass them to Recognize. I'm probably missing something obvious here.
    * Missing tests (will be added)
    
    * Linter fixes
    
    * Adapt it to the new API
    
    * Send the full path to process
    
    * Simplify import process
    
    * Add datasource description
    
    * Use the URL as the item content
    
    * Add basic tests
    
    * Give the test some more time
    
    * Do not return an error if context was cancelled

---
+++ datasources/firefox/firefox.go
@@ -0,0 +1,174 @@
+/*
+	Timelinize
+	Copyright (c) 2024 Sergio Rubio
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published
+	by the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// Package firefox implements a data source that imports visited sites
+// from Firefox's places.sqlite database.
+package firefox
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/timelinize/timelinize/timeline"
+	"go.uber.org/zap"
+)
+
+// Data source name and ID.
+const (
+	DataSourceName = "Firefox"
+	DataSourceID   = "firefox"
+)
+
+func init() {
+	err := timeline.RegisterDataSource(timeline.DataSource{
+		Name:            DataSourceID,
+		Title:           DataSourceName,
+		Icon:            "firefox.svg",
+		Description:     "Firefox places.sqlite database importer",
+		NewFileImporter: func() timeline.FileImporter { return new(Firefox) },
+	})
+	if err != nil {
+		timeline.Log.Fatal("registering data source", zap.Error(err))
+	}
+}
+
+// Firefox interacts with the places.sqlite database to get visited sites.
+type Firefox struct{}
+
+// Recognize returns whether the input file is recognized.
+func (Firefox) Recognize(_ context.Context, dirEntry timeline.DirEntry, _ timeline.RecognizeParams) (timeline.Recognition, error) {
+	if filepath.Base(dirEntry.Name()) == "places.sqlite" {
+		return timeline.Recognition{Confidence: 1}, nil
+	}
+
+	return timeline.Recognition{}, nil
+}
+
+// FileImport conducts an import of the data using this data source.
+func (f *Firefox) FileImport(ctx context.Context, dirEntry timeline.DirEntry, params timeline.ImportParams) error {
+	return f.process(ctx, dirEntry.FullPath(), params.Pipeline)
+}
+
+// process reads the places.sqlite database and sends the items to the channel.
+func (f *Firefox) process(ctx context.Context, path string, itemChan chan<- *timeline.Graph) error {
+	tempDir, err := os.MkdirTemp("", "firefox_import_*")
+	if err != nil {
+		return fmt.Errorf("creating temp directory: %w", err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	tmpDB := filepath.Join(tempDir, filepath.Base(path))
+	// copyFile copies the contents of the source file to the destination file.
+	copyFile := func(src, dst string) error {
+		sourceFile, err := os.Open(src)
+		if err != nil {
+			return fmt.Errorf("opening source file: %w", err)
+		}
+		defer sourceFile.Close()
+
+		destFile, err := os.Create(dst)
+		if err != nil {
+			return fmt.Errorf("creating destination file: %w", err)
+		}
+		defer destFile.Close()
+
+		_, err = io.Copy(destFile, sourceFile)
+		if err != nil {
+			return fmt.Errorf("copying file contents from %s to %s: %w", src, dst, err)
+		}
+
+		return nil
+	}
+	err = copyFile(path, tmpDB)
+	if err != nil {
+		return fmt.Errorf("copying file to temp directory: %w", err)
+	}
+
+	// Open the database in read-only mode
+	db, err := sql.Open("sqlite3", fmt.Sprintf("file:%s?mode=ro", tmpDB))
+	if err != nil {
+		return fmt.Errorf("opening database in read-only mode: %w", err)
+	}
+	defer db.Close()
+
+	// Verify that we can read from the database
+	err = db.Ping()
+	if err != nil {
+		return fmt.Errorf("verifying database connection: %w", err)
+	}
+
+	rows, err := db.QueryContext(ctx, `
+		SELECT p.url, p.title, p.description, h.visit_date
+		FROM moz_places p
+		INNER JOIN moz_historyvisits h
+		ON p.id = h.place_id
+	`)
+	if err != nil {
+		return fmt.Errorf("querying database: %w", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var url string
+		var title, description sql.NullString
+		var visitDate int64
+
+		err := rows.Scan(&url, &title, &description, &visitDate)
+		if err != nil {
+			return fmt.Errorf("scanning row: %w", err)
+		}
+
+		timestamp := time.Unix(0, visitDate*1000) //nolint:mnd // Convert microseconds to nanoseconds
+
+		select {
+		case <-ctx.Done():
+			if ctx.Err() == context.Canceled {
+				return nil
+			}
+			return ctx.Err()
+		default:
+			item := &timeline.Item{
+				Classification:       timeline.ClassPageView,
+				Timestamp:            timestamp,
+				IntermediateLocation: path,
+				Content: timeline.ItemData{
+					Data: timeline.StringData(url),
+				},
+				Metadata: timeline.Metadata{
+					"URL":         url,
+					"Title":       title.String,
+					"Visit date":  timestamp.Format("2006-01-02 15:04:05"),
+					"Description": description.String,
+				},
+			}
+
+			itemChan <- &timeline.Graph{Item: item}
+		}
+	}
+
+	if err := rows.Err(); err != nil {
+		return fmt.Errorf("row iteration error: %w", err)
+	}
+
+	return nil
+}
