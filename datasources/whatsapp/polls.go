3670ab8371c2e11b71095a1d6040a77f0a181011
JP Hastings-Spital <jphastings@gmail.com>
2025-05-03 23:43:32 +0100

    WhatsApp: Correct Poll Structure & fix parsing
    I had incorrect POLL lines in the test fixtures; this commit fixes them, and the importer so it can read them properly.

--- datasources/whatsapp/polls.go
+++ datasources/whatsapp/polls.go
@@ -3,15 +3,14 @@ package whatsapp
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 
 	"github.com/timelinize/timelinize/timeline"
 )
 
 // We use `\p{Lu}+` to match the "POLL" and "OPTION" words in any locale (eg. OPCIÓN)
-const optionRegexStr = `\p{Lu}+: (.+) \((\d) .+\)`
-
-var optionRegex = regexp.MustCompile(optionRegexStr)
-var pollRegex = regexp.MustCompile(`\p{Lu}+:\r\n(.+)((\r\n` + optionRegexStr + `)+)`)
+var pollRegex = regexp.MustCompile(`^\p{Lu}+:\r\n(.+)\r\n$`)
+var optionRegex = regexp.MustCompile(`^\p{Lu}+: (.+) \((\d+) .+\)\r\n$`)
 
 func extractPoll(content []string) (string, timeline.Metadata, bool) {
 	if len(content) < 2 {
@@ -26,11 +25,20 @@ func extractPoll(content []string) (string, timeline.Metadata, bool) {
 	meta := make(timeline.Metadata)
 	meta["Poll Question"] = parts[1]
 	customMessage := parts[1]
-	for i, opt := range optionRegex.FindAllStringSubmatch(parts[2], -1) {
-		meta[fmt.Sprintf("Poll Option %d", i+1)] = opt[1]
-		meta[fmt.Sprintf("Poll Votes %d", i+1)] = opt[2]
 
-		customMessage += "\r\n- " + opt[1] + " (☑︎ " + opt[2] + ")"
+	for i, optLine := range content[2:] {
+		parts := optionRegex.FindStringSubmatch(optLine)
+		if parts == nil {
+			return "", nil, false
+		}
+
+		customMessage += "\r\n- " + parts[1] + " (☑︎ " + parts[2] + ")"
+
+		// No need to track error, it's extracted with `\d+`
+		voteCount, _ := strconv.Atoi(parts[2])
+
+		meta[fmt.Sprintf("Poll Option %d", i+1)] = parts[1]
+		meta[fmt.Sprintf("Poll Votes %d", i+1)] = voteCount
 	}
 
 	return customMessage, meta, true
