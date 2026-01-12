2063b9e308e7439d16d2d115e58271c1eac4cbca
TherionAcribus <91415785+TherionAcribus@users.noreply.github.com>
2026-01-12 22:39:05 +0100

    frontend: Add svg for document (#182)

--- frontend/resources/js/common-lib.js
+++ frontend/resources/js/common-lib.js
@@ -106,7 +106,13 @@ const tlz = {
 
 		page_view: `<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
 		<path d="M12 8l0 4l2 2" /><path d="M3.05 11a9 9 0 1 1 .5 4m-.5 5v-5h5" />
-		`
+		`,
+		document: `
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+            <path d="M9 17h6"></path>
+            <path d="M9 13h6"></path>`,
 	},
 
 	itemClassIconAndLabel(item, pathOnly) {
@@ -285,7 +291,11 @@ tlz.itemClassIcons = {
 	page_view: `
 		<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-history">
 			${tlz.itemClassIconPaths.page_view}
-		</svg>`
+		</svg>`,
+	document: `
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-description" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            ${tlz.itemClassIconPaths.document}
+        </svg>`,
 };
 
 // set all the predefined intervals
